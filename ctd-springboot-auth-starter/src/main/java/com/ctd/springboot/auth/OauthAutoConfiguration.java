3b3211f07db12ef6316ee0b1bcf0ecf97b652d87
chentudonggit <1554862674@qq.com>
2020-04-17 23:54:37 +0800

    添加扫描类

---
+++ ctd-springboot-auth-starter/src/main/java/com/ctd/springboot/auth/OauthAutoConfiguration.java
@@ -0,0 +1,17 @@
+package com.ctd.springboot.auth;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * OauthAutoConfiguration
+ *
+ * @author chentudong
+ * @date 2020/4/17 23:48
+ * @since 1.0
+ */
+@Configuration
+@ComponentScan
+public class OauthAutoConfiguration
+{
+}
