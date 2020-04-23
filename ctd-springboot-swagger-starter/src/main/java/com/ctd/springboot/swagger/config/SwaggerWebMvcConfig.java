53f1e44c80790e0dbdb7362bfbf95896a6a7c0ac
chentudonggit <1554862674@qq.com>
2020-04-23 23:14:13 +0800

    添加swagger config

---
+++ ctd-springboot-swagger-starter/src/main/java/com/ctd/springboot/swagger/config/SwaggerWebMvcConfig.java
@@ -0,0 +1,34 @@
+package com.ctd.springboot.swagger.config;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+
+/**
+ * SwaggerWebMvcConfig
+ *
+ * @author chentudong
+ * @date 2020/3/28 13:04
+ * @since 1.0
+ */
+public class SwaggerWebMvcConfig extends WebMvcConfigurationSupport
+{
+    /**
+     * 设置资源文件目录
+     *
+     * @param registry registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry)
+    {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/")
+                .addResourceLocations("classpath:/resources/")
+                .addResourceLocations("classpath:/public/");
+        super.addResourceHandlers(registry);
+    }
+}
