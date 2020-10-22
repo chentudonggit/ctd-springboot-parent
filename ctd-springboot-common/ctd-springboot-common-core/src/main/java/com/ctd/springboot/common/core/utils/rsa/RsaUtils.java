3ee1c97883325d4d3d6d604687b5c2ba72b31be6
chentudonggit <1554862674@qq.com>
2020-10-22 19:04:56 +0800

    修改rsa

--- ctd-springboot-common/ctd-springboot-common-core/src/main/java/com/ctd/springboot/common/core/utils/rsa/RsaUtils.java
+++ ctd-springboot-common/ctd-springboot-common-core/src/main/java/com/ctd/springboot/common/core/utils/rsa/RsaUtils.java
@@ -1,97 +1,205 @@
 package com.ctd.springboot.common.core.utils.rsa;
 
+import org.apache.commons.codec.binary.Base64;
 import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
 
 import javax.crypto.Cipher;
-import java.security.KeyFactory;
-import java.security.PrivateKey;
-import java.security.PublicKey;
+import java.math.BigInteger;
+import java.security.*;
+import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.RSAPrivateKeySpec;
+import java.security.spec.RSAPublicKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 
 /**
- * RsaUtils
+ * 加密工具
  *
  * @author chentudong
- * @date 2020/3/7 20:29
- * @since 1.0
+ * @date 2019/8/20 21:11
  */
 public final class RsaUtils {
+
     private static final String CIPHER_INSTANCE = "RSA/ECB/PKCS1Padding";
     private static final String RSA = "RSA";
 
+    private static final KeyPair KEY_PAIR = initKey();
+
+    private static final String RM = "1485911093980466539602251665617432573407094000417447004282924779707911060961966661" +
+            "77857622373761787549416835748972964450253950685016373794898455705722054144000951974374894836815944566223251" +
+            "225005475733338880518270152564313806057014117937280775741076170704355375933801070732869063448839888298721357589130064073";
+    private static final String RE = "4940642484234607376668548460483310680549000619969877728661552081208338790213500088" +
+            "01393937645397731432525484505831800103069081492468651504437531208842223678146045722759843529259848363093552" +
+            "91957135659316198844588118790243734652643395389156065029573103258872818271296403492500974672509805784899854018633986413";
+    private static RSAPrivateKey DEFAULT_PRIVATE_KEY = getPrivateKey(RM, RE);
+
     /**
-     * 公钥加密
-     *
-     * @param content   要加密的内容
-     * @param publicKey 公钥
+     * 用默认的公锁加密
      */
-    public static String encrypt(String content, PublicKey publicKey) {
-        try {
-            Cipher cipher = Cipher.getInstance(CIPHER_INSTANCE);
-            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-            byte[] output = cipher.doFinal(content.getBytes());
-            BASE64Encoder encoder = new BASE64Encoder();
-            return encoder.encode(output);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
+    public static String encryptBase64Default(String string) {
+        return encryptBase64(string, DEFAULT_PUBLIC_KEY);
+    }
+
+    private static final String PM = "1485911093980466539602251665617432573407094000417447004282924779707911060961966661" +
+            "77857622373761787549416835748972964450253950685016373794898455705722054144000951974374894836815944566223251" +
+            "225005475733338880518270152564313806057014117937280775741076170704355375933801070732869063448839888298721357589130064073";
+    private static final String PE = "65537";
+    private static RSAPublicKey DEFAULT_PUBLIC_KEY = getPublicKey(PM, PE);
+
+
+    /**
+     * 加密
+     *
+     * @param string string
+     * @return String
+     */
+    public static String encryptBase64(String string) {
+        return new String(Base64.encodeBase64(encrypt(string.getBytes())));
     }
 
     /**
-     * 公钥加密
-     *
-     * @param content   要加密的内容
-     * @param publicKey 公钥
+     * 用默认的私钥解密
      */
-    public static byte[] encrypt(byte[] content, PublicKey publicKey) {
-        try {
-            Cipher cipher = Cipher.getInstance(CIPHER_INSTANCE);
-            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-            return cipher.doFinal(content);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
+    public static String decryptBase64Default(String string) {
+        return decryptBase64(string, DEFAULT_PRIVATE_KEY);
     }
 
     /**
-     * 私钥解密
+     * 用私钥解密
      *
-     * @param content    要解密的内容
-     * @param privateKey 私钥
+     * @param string string
+     * @param prk    prk
+     * @return String
      */
-    public static byte[] decrypt(byte[] content, PrivateKey privateKey) {
+    public static String decryptBase64(String string, RSAPrivateKey prk) {
         try {
-            Cipher cipher = Cipher.getInstance(CIPHER_INSTANCE);
-            cipher.init(Cipher.DECRYPT_MODE, privateKey);
-            return cipher.doFinal(content);
+            Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+            Cipher cipher = Cipher.getInstance("RSA/None/PKCS1Padding", "BC");
+
+            cipher.init(Cipher.DECRYPT_MODE, prk);
+            byte[] plainText = cipher.doFinal(Base64.decodeBase64(string));
+            return new String(plainText);
         } catch (Exception e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
-        return null;
     }
 
     /**
-     * 私钥解密
-     *
-     * @param content    要解密的内容
-     * @param privateKey 私钥
+     * 用公匙加密
      */
-    public static String decrypt(String content, PrivateKey privateKey) {
+    public static String encryptBase64(String string, PublicKey pbk) {
         try {
-            Cipher cipher = Cipher.getInstance(CIPHER_INSTANCE);
-            cipher.init(Cipher.DECRYPT_MODE, privateKey);
-            byte[] b = cipher.doFinal(content.getBytes());
-            BASE64Encoder encoder = new BASE64Encoder();
-            return encoder.encode(b);
+            Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+            Cipher cipher = Cipher.getInstance("RSA/None/PKCS1Padding", "BC");
+            cipher.init(Cipher.ENCRYPT_MODE, pbk);
+            byte[] plainText = cipher.doFinal(string.getBytes());
+            return new String(Base64.encodeBase64(plainText));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static byte[] encrypt(byte[] string) {
+        try {
+            Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+            Cipher cipher = Cipher.getInstance("RSA/None/PKCS1Padding", "BC");
+            PublicKey pbk = KEY_PAIR.getPublic();
+            cipher.init(Cipher.ENCRYPT_MODE, pbk);
+            return cipher.doFinal(string);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * getPublicKey
+     *
+     * @param modulus  modulus
+     * @param exponent exponent
+     * @return RSAPublicKey
+     */
+    public static RSAPublicKey getPublicKey(String modulus, String exponent) {
+        try {
+            BigInteger b1 = new BigInteger(modulus);
+            BigInteger b2 = new BigInteger(exponent);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            RSAPublicKeySpec keySpec = new RSAPublicKeySpec(b1, b2);
+            return (RSAPublicKey) keyFactory.generatePublic(keySpec);
         } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
-        return null;
+    }
+
+    private static KeyPair initKey() {
+        try {
+            Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+            SecureRandom random = new SecureRandom();
+            random.setSeed(System.currentTimeMillis());
+            KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA", "BC");
+            generator.initialize(1024, random);
+            return generator.generateKeyPair();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 获取私匙
+     *
+     * @param modulus  modulus
+     * @param exponent exponent
+     * @return RSAPrivateKey
+     */
+    public static RSAPrivateKey getPrivateKey(String modulus, String exponent) {
+        try {
+            BigInteger b1 = new BigInteger(modulus);
+            BigInteger b2 = new BigInteger(exponent);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            RSAPrivateKeySpec keySpec = new RSAPrivateKeySpec(b1, b2);
+            return (RSAPrivateKey) keyFactory.generatePrivate(keySpec);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 公匙
+     *
+     * @return String
+     */
+    public static String base64PublicKey() {
+        return new String(Base64.encodeBase64(KEY_PAIR.getPublic().getEncoded()));
+    }
+
+    /**
+     * 私匙
+     *
+     * @return String
+     */
+    public static String base64PrivateKey() {
+        return new String(Base64.encodeBase64(KEY_PAIR.getPrivate().getEncoded()));
+    }
+
+    /**
+     * 获取Default公匙
+     *
+     * @return String
+     */
+    public static String base64PublicKeyDefault() {
+        return new String(Base64.encodeBase64(DEFAULT_PUBLIC_KEY.getEncoded()));
+    }
+
+    /**
+     * 获取Default私匙
+     *
+     * @return String
+     */
+    public static String base64PrivateKeyDefault() {
+        return new String(Base64.encodeBase64(DEFAULT_PRIVATE_KEY.getEncoded()));
     }
 
     /**
