c65ec31aeaec4617963cd4397ab286cee17d669b
chentudonggit <1554862674@qq.com>
2020-11-13 23:06:47 +0800

    添加枚举

---
+++ ctd-springboot-redis-starter/src/main/java/com/ctd/springboot/redis/enums/CacheStorageTypeEnum.java
@@ -0,0 +1,20 @@
+package com.ctd.springboot.redis.enums;
+
+/**
+ * CacheStorageTypeEnum
+ *
+ * @author chentudong
+ * @date 2020/11/13 12:01
+ * @since 1.0
+ */
+public enum CacheStorageTypeEnum {
+    /**
+     * 内存
+     */
+    Memory,
+
+    /**
+     * redis
+     */
+    Redis;
+}
